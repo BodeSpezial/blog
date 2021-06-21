2437620df862bb5ac2813514baa5a38b1e5c129d
BodeSpezial <konstantin.bode@outlook.com>
2021-06-22 00:46:13 +0200

    Add build comments

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # clean up the build folder
+echo "clean public folder"
 rm -rf public
 
 # build the site
+echo "build website"
 hugo
 
 # push the site to the cloud instance
@@ -11,8 +13,10 @@ hugo
 # include the files [r]ecursively, give [v]erbose output
 # delete files that don't exist in the local folder
 # [a]rchive and [z] compress the files for the transfer
+echo "put site on server"
 rsync --delete -avzr public/* uberspace:html/
 
 # delete the build folder
+echo "clean public folder"
 rm -rf public
 
